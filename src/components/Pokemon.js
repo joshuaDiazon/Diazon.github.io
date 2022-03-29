5c114e55cdc91e1aa9b40d3c2485ebb86fb8b86f
Joshua Diazon <joshuadiazon@gmail.com>
2022-03-29 19:21:38 +0800

    pokemons

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,18 +2,20 @@ import { Button, Card} from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { deletePokemon } from "./feature/userSlice/PokemonSlice";
 import { addToFav } from "./feature/userSlice/FavoriteSlice";
+import { useNavigate } from "react-router-dom";
 
 const Pokemon = () => {
   const pokemons = useSelector((state) => state.pokemonData.data);
-
   const dispatch = useDispatch();
+  const navigate = useNavigate()
 
   const handleAdd = (list) => {
     dispatch(addToFav(list));
+    navigate("/fav")
   };
 
   return (
-    <Card>
+    <Card className="card">
       {pokemons.map((list) => {
         return (
           <div className="pokemon">
